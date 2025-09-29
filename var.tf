86fb33f2b7ebb4eb52c1222cde6c5ef32bc21728
pon802283 <164842058+pon802283@users.noreply.github.com>
2025-09-29 14:27:25 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
 variable "region" {
     default = "us-east-1"
 }
-####################################
-##############|VPC|##############
-####################################
+#####################################
+################|VPC|################
+#####################################
 variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
@@ -37,6 +37,10 @@ variable "pvt-sub1-cidr" {
 variable "pvt-sub2-cidr" {
     default = "10.0.4.0/24"
 }
+####################################
+############|DATA BASE|#############
+####################################
+
 ##############|DB USER & PASSWORD|##############
 variable "db-username" {
     default = "admin"
@@ -45,14 +49,20 @@ variable "db-password" {
     default = "your password"
     sensitive = true
 }
+#####################################
+################|EC2|################
+#####################################
+
 ##############|AMI|##############
 variable "ami" {
     default = "ami-00ca32bbc84273381" 
 }
+
 ##############|Instance Type|##############
 variable "instance_type" {
      default = "t3.nano"
 }
+
 ##############|KEY PARI|##############
 variable "key_pari" {
     default = "your key"
